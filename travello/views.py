53e2c4b61044285b94299534484d0a177a4ea33c
8140171224 <black8140171224@gmail.com>
2019-07-06 20:03:12 +0530

    travello 1

---
+++ travello/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Destination
+# Create your views here.
+
+def index(request):
+
+    dests = Destination.objects.all()
+
+    return render(request, 'index.html', {'dests': dests })
+def about(request):
+    return render(request, 'about.html')
